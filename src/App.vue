4302d79fc1c797ae5d797fa979868ee5ed32ccd6
Renata Quintela <vihquintela0604@gmail.com>
2022-05-06 11:02:36 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import HeaderMenu from "@/components/HeaderMenu.vue";
+
+export default {
+  components: { HeaderMenu },
+};
+</script>
+<template>
+  <HeaderMenu />
+  <header id="header">Cabeçalho</header>
+  <main id="content">Principal</main>
+  <footer id="footer">Rodapé</footer>
+</template>
+<style></style>
---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+<h2>alegria</h2>
+</template>
+
+<style></style>
